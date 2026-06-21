74ee4795c49952414fa9966b12ccbe434b2e06a3
Lee Sang Hun <114682353+hoooni10099@users.noreply.github.com>
2026-06-22 08:54:27 +0900

    Create contunue.c

---
+++ C/contunue.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, sum = 0;
+
+    for (i = 1; i <= 100; i++) {
+        if (i % 3 ==0) {
+            continue;
+        }
+        sum += i;
+        //printf("%d\n", i);
+    }
+    printf("합계: %d\n", sum);
+    return 0;
+}
