c5a30590c9d9fcd0fd77d940587c4f0e001a79f2
Lee Sang Hun <114682353+hoooni10099@users.noreply.github.com>
2026-07-20 16:31:08 +0900

    Create AddNumber.java

---
+++ Java/AddNumber.java
@@ -0,0 +1,10 @@
+public class AddNumber {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+
+        int result = a + b;
+
+        System.out.println("result = " + result);
+    }
+}
