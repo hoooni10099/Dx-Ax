fe7198a17bf7a08f6eefe82c0c2e65e4929e16dd
Lee Sang Hun <114682353+hoooni10099@users.noreply.github.com>
2026-07-21 12:02:18 +0900

    Create CalculatorTest.java

---
+++ Java/CalculatorTest.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int value = sc.nextInt();
+        System.out.println("입력 받은 정수값은 : " + value);
+    }
+}
