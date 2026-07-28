f24986a6ad72540b7b4c73131e48520bc4138e36
Lee Sang Hun <114682353+hoooni10099@users.noreply.github.com>
2026-07-28 13:58:35 +0900

    Update master_data.sql

--- Mini MES Project/actuator-mini-mes/sql/master_data.sql
+++ Mini MES Project/actuator-mini-mes/sql/master_data.sql
@@ -1,1 +1,21 @@
-d
+INSERT INTO item (
+    item_code,
+    item_name,
+    item_type
+)
+VALUES
+    ('ACT-BASIC',  '기본형 전동 액추에이터', 'PRODUCT'),
+    ('ACT-SENSOR', '센서형 전동 액추에이터', 'PRODUCT'),
+    ('MAT-MOTOR',  'DC 모터',                'MATERIAL'),
+    ('MAT-GEAR',   '감속기',                 'MATERIAL'),
+    ('MAT-HOUSING','하우징',                 'MATERIAL'),
+    ('MAT-PCB',    'PCB',                    'MATERIAL'),
+    ('MAT-SENSOR', '위치센서',               'MATERIAL');
+
+
+# is_active 비활성화
+UPDATE item
+SET is_active = 0
+WHERE item_code = 'MAT-SENSOR';
+---------------------------------------------------------
+
